44dc02e11e22854254121220358a6816f6109a3c
gyp03236 <gyp03236@naver.com>
2022-01-16 15:04:05 +0900

    style: 일단 수정된 거 같아 보이는 StudyBoardController 업로드
    2022.01.16 StudyBoardController.java 추가

--- escahp/src/main/java/com/esca/escahp/controller/StudyBoardController.java
+++ escahp/src/main/java/com/esca/escahp/controller/StudyBoardController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("/notice")
+@RequestMapping("/study")
 @RequiredArgsConstructor
 public class StudyBoardController {
 
