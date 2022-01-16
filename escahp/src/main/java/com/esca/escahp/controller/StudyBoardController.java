335d9685a7bf0bb387eccad27303d1c94285edac
gyp03236 <gyp03236@naver.com>
2022-01-16 17:12:24 +0900

    merge: studyboard 추가2

--- escahp/src/main/java/com/esca/escahp/controller/StudyBoardController.java
+++ escahp/src/main/java/com/esca/escahp/controller/StudyBoardController.java
@@ -1,4 +1,78 @@
 package com.esca.escahp.controller;
 
+import com.esca.escahp.dto.StudyBoardDto;
+import com.esca.escahp.service.I_StudyBoardService;
+import java.net.URI;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/study")
+@RequiredArgsConstructor
 public class StudyBoardController {
+
+	private final I_StudyBoardService studyBoardService;
+
+	@GetMapping
+	public ResponseEntity<List<StudyBoardDto>> getAllStudyBoard() {
+		List<StudyBoardDto> studyBoard = studyBoardService.getStudyBoardList();
+		return ResponseEntity.ok().body(studyBoard);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<StudyBoardDto> getStudyBoardById(@PathVariable long id){
+		StudyBoardDto studyBoardDto = studyBoardService.selectStudyBoard(id);
+		if(studyBoardDto == null){
+			return ResponseEntity.noContent().build();
+		}
+
+		studyBoardService.updateViewCnt(id);
+		return ResponseEntity.ok().body(studyBoardDto);
+	}
+
+	@PostMapping
+	public ResponseEntity<StudyBoardDto> insertStudyBoard(@RequestBody StudyBoardDto studyBoardDto){
+		studyBoardService.addBoard(studyBoardDto);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(studyBoardDto.getId())
+			.toUri();
+
+		return ResponseEntity.created(location).build();
+
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<StudyBoardDto> updateStudyBoard(@PathVariable Long id, @RequestBody StudyBoardDto studyBoardDto){
+		studyBoardDto.setId(id);
+		studyBoardService.updateBoard(studyBoardDto);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+			.buildAndExpand()
+			.toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<Object> deleteAction(@PathVariable Long id, @RequestBody StudyBoardDto studyBoardDto){
+		studyBoardDto.setId(id);
+		studyBoardService.deleteBoard(studyBoardDto);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+			.buildAndExpand()
+			.toUri();
+
+		return ResponseEntity.created(location).build();
+
+	}
 }
--- escahp/src/main/java/com/esca/escahp/dto/StudyBoardDto.java
+++ escahp/src/main/java/com/esca/escahp/dto/StudyBoardDto.java
@@ -1,4 +1,45 @@
 package com.esca.escahp.dto;
 
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class StudyBoardDto {
+	// pk
+	private Long id;
+
+	// 카테고리
+	private String category;
+
+	// 제목
+	private String title;
+
+	// 내용
+	private String content;
+
+	// 유저아이디
+	private String writer;
+
+	// 첨부파일
+	private String file;
+
+	// 작성일자
+	private LocalDateTime createdAt;
+
+	// 수정일자
+	private LocalDateTime updatedAt;
+
+	// 삭제일자
+	private LocalDateTime deletedAt;
+
+	// 삭제여부
+	private String deleteYn;
+
+	// 조회수
+	private long viewCnt;
+
+	private int likes;
+
 }
