48bf5594197e9216f516dd4441eb4e401a31196a
DESKTOP-KVCQHCD\multicampus <gyp03236@naver.com>
2022-04-27 21:32:57 +0900

    feat: UserDto 추가

---
+++ escahp/src/main/java/com/esca/escahp/dto/UserDto.java
@@ -0,0 +1,19 @@
+package dto;
+
+public class UserDto {
+
+    private Long id;
+
+    private String userId;
+
+    private String name;
+
+    private String password;
+
+    private String nickname;
+
+    private Integer generation;
+
+    private Integer rank;
+
+}
