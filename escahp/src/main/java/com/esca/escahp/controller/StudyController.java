2c1518f2b8c60ec3e9c4f49f9b6ee07c1a9c5a66
DESKTOP-KVCQHCD\multicampus <gyp03236@naver.com>
2022-06-21 16:38:18 +0900

    feat : StudyController refactoring

--- escahp/src/main/java/com/esca/escahp/controller/StudyController.java
+++ escahp/src/main/java/com/esca/escahp/controller/StudyController.java
@@ -1,16 +1,24 @@
 package com.esca.escahp.controller;
 
+import com.esca.escahp.domain.StudyBoard;
 import com.esca.escahp.dto.StudyBoardDto;
 import com.esca.escahp.dto.response.StudyResponse;
 import com.esca.escahp.service.StudyService;
 import io.swagger.annotations.ApiOperation;
+import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/study")
@@ -30,4 +38,57 @@ public class StudyController {
         return ResponseEntity.ok().body(studyBoard);
     }
 
+    @ApiOperation(value = "id에 해당하는 게시물 정보 반환")
+    @GetMapping("/{id}")
+    public ResponseEntity<StudyResponse> getStudyBoardById(@PathVariable long id) {
+        StudyResponse result = studyService.selectStudyBoard(id);
+        if (result == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        studyService.updateViewCnt(id);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @ApiOperation(value = "게시물 객체 추가")
+    @PostMapping
+    public ResponseEntity<StudyResponse> insertStudyBoard(
+        @RequestBody StudyBoard studyBoard) {
+        studyService.addBoard(studyBoard);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(studyBoard.getId())
+            .toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+
+    @ApiOperation(value = "id에 해당하는 게시물 정보 수정")
+    @PutMapping("/{id}")
+    public ResponseEntity<StudyResponse> updateStudyBoard(@PathVariable Long id,
+        @RequestBody StudyBoard studyBoard) {
+        studyBoard.setId(id);
+        studyService.updateBoard(studyBoard);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+            .buildAndExpand()
+            .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @ApiOperation(value = "id에 해당하는 게시물 정보 삭제")
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> deleteAction(@PathVariable Long id,
+        @RequestBody StudyBoard studyBoard) {
+        studyBoard.setId(id);
+        studyService.deleteBoard(studyBoard);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+            .buildAndExpand()
+            .toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
 }
