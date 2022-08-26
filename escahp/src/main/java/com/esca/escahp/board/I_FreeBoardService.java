db298d57a199e5d589f4a58f72fa9cc4e2bba810
DyeonKim <vlxjdus@gmail.com>
2022-08-26 21:28:32 +0900

    refactor: 자유게시판 service, controller 수정

--- escahp/src/main/java/com/esca/escahp/board/I_FreeBoardService.java
+++ escahp/src/main/java/com/esca/escahp/board/I_FreeBoardService.java
@@ -1,14 +1,18 @@
-package com.esca.escahp.service;
+package com.esca.escahp.board;
 
-import com.esca.escahp.dto.FreeBoardDto;
+import com.esca.escahp.board.dto.FreeRequest;
+import com.esca.escahp.board.dto.FreeResponse;
 import java.util.List;
 
 
 public interface I_FreeBoardService {
-    public FreeBoardDto getArticle(long no);
-    public List<FreeBoardDto> getArticles();
-    public void writeArticle(FreeBoardDto dto);
-    public void modifyArticle(FreeBoardDto dto);
-    public void deleteArticle(FreeBoardDto dto);
-    public void updateReport(FreeBoardDto dto);
+    List<FreeResponse> getFreeBoardList();
+    FreeResponse selectFreeBoard(long id);
+    Long postBoard(FreeRequest f);
+    void updateBoard(Long id, FreeRequest f);
+    void deleteBoard(Long id);
+    void increaseViewCnt(long id);
+
+    // 좋아요, 신고는 회원정보와 같이
+    //public void updateReport(long id);
 }
