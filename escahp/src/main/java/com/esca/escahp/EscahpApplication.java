82440ef54f6b418b1d49a273fbd544f7df3afb57
RinSabbit <celena1004dev@gmail.com>
2022-09-04 19:40:29 +0900

    test

--- escahp/src/main/java/com/esca/escahp/EscahpApplication.java
+++ escahp/src/main/java/com/esca/escahp/EscahpApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class EscahpApplication {
 	public static void main(String[] args) {
+		System.out.println("Hello World!");
 		SpringApplication.run(EscahpApplication.class, args);
 	}
 }
