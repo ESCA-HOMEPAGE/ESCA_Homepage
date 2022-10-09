ab130c9920b3ee4fb176d6c9023af03114bb7559
gyp03236 <gyp03236@naver.com>
2022-10-09 16:27:35 +0900

    feat: Login 기능 추가 - JWT 발급기능 추가

---
+++ escahp/src/main/java/com/esca/escahp/user/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.esca.escahp.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
+
+	String userId;
+	String password;
+	String jwtToken;
+}
--- escahp/src/main/java/com/esca/escahp/user/entity/User.java
+++ escahp/src/main/java/com/esca/escahp/user/entity/User.java
@@ -1,19 +1,30 @@
 package com.esca.escahp.user.entity;
 
 import com.esca.escahp.user.dto.AuthRequest;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Entity
 @Setter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue
@@ -46,6 +57,10 @@ public class User {
 	@Column
 	private String pr;
 
+	@ElementCollection(fetch = FetchType.EAGER)
+	@Builder.Default
+	private List<String> roles = new ArrayList<>();
+
 	public User(AuthRequest auth){
 		this.id = auth.getId();
 		this.userId = auth.getUserId();
@@ -61,5 +76,40 @@ public class User {
 
 	public void updatePassword(String newPassword) {
 		this.password = newPassword;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+	}
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Override
+	public String getUsername() {
+		return this.userId;
+	}
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
