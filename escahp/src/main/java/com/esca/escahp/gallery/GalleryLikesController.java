d9b77afa3acdc3a2a2766c12709bf009ee8fd2b9
sihwa1019 <epretty@naver.com>
2022-10-09 17:04:53 +0900

    feat: 좋아요 기능 1차완료

--- escahp/src/main/java/com/esca/escahp/gallery/GalleryLikesController.java
+++ escahp/src/main/java/com/esca/escahp/gallery/GalleryLikesController.java
@@ -1,9 +1,14 @@
 package com.esca.escahp.gallery;
 
+import com.esca.escahp.gallery.dto.GalleryLikeRequest;
 import com.esca.escahp.gallery.dto.GalleryLikeResponse;
+import com.esca.escahp.gallery.dto.GalleryRequest;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -16,5 +21,14 @@ public class GalleryLikesController {
     public ResponseEntity<List<GalleryLikeResponse>> getGalleryLikes(){
         List<GalleryLikeResponse> galleryLikes = galleryLikesService.getGalleryLikesList();
         return ResponseEntity.ok().body(galleryLikes);
+    }
+
+    @ApiOperation(value = "갤러 게시판의 좋아요 추가")
+    @PostMapping
+    public ResponseEntity<Long> insertGalleryLikes(
+            @RequestBody GalleryLikeRequest galleryLikeRequest){
+        Long id = galleryLikesService.addGalleryLikes(galleryLikeRequest);
+
+        return ResponseEntity.ok(id);
     }
 }
--- escahp/src/main/java/com/esca/escahp/gallery/I_GalleryLikesService.java
+++ escahp/src/main/java/com/esca/escahp/gallery/I_GalleryLikesService.java
@@ -1,10 +1,12 @@
 package com.esca.escahp.gallery;
 
+import com.esca.escahp.gallery.dto.GalleryLikeRequest;
 import com.esca.escahp.gallery.dto.GalleryLikeResponse;
 
 import java.util.List;
 
 public interface I_GalleryLikesService {
-    void countLikes(long id);
+
     List<GalleryLikeResponse> getGalleryLikesList();
+    Long addGalleryLikes(GalleryLikeRequest b);
 }
