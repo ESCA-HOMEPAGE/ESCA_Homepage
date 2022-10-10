7cbbf0e55dc1b818fd46b66b5f50d68391eb214f
RinSabbit <celena1004dev@gmail.com>
2022-10-10 20:27:05 +0900

    refactor: modify user api

--- escahp/src/main/java/com/esca/escahp/user/UserController.java
+++ escahp/src/main/java/com/esca/escahp/user/UserController.java
@@ -1,17 +1,18 @@
 package com.esca.escahp.user;
 
+import com.esca.escahp.user.dto.ChangePasswordRequest;
+import com.esca.escahp.user.dto.FindRequest;
 import com.esca.escahp.user.dto.UserRequest;
 import com.esca.escahp.user.dto.UserResponse;
-import com.esca.escahp.user.dto.ChangePasswordRequest;
 import com.esca.escahp.user.entity.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Api(tags = {"Auth"})
+@Api(tags = {"User"})
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users")
 @CrossOrigin(origins = {"*"})
 public class UserController {
 
@@ -33,14 +34,14 @@ public class UserController {
     }
 
     @ApiOperation(value = "아이디 찾기")
-    @GetMapping("/find-id")
-    public ResponseEntity<String> findId(@RequestParam("name") String name, @RequestParam("email") String email) {
-        String result = userService.findUserId(name, email);
+    @GetMapping("/help")
+    public ResponseEntity<String> findId(@RequestBody FindRequest request) {
+        String result = userService.findUserId(request.getName(), request.getEmail());
         return ResponseEntity.ok().body(result);
     }
 
     @ApiOperation(value = "사용자 자의에 의한 비밀번호 변경")
-    @PutMapping("/change-password")
+    @PutMapping("/help/myInfo")
     public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordRequest request) {
         userService.resetPassword(
                 request.getUserId(),
@@ -52,7 +53,7 @@ public class UserController {
     }
 
     @ApiOperation(value = "분실한 비밀번호 재설정")
-    @PutMapping("/reset-password")
+    @PutMapping("/help/myInfo")
     public ResponseEntity<Void> resetPassword(@RequestBody String userId) {
         userService.resetPassword(userId);
         return ResponseEntity.noContent().build();
