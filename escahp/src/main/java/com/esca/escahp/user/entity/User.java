f4fd01a57d784bb671b943238c067a2a359a4580
jueunlee0920 <jueunlee0920@kidaristudio.com>
2022-10-16 16:19:54 +0900

    import 추가

--- escahp/src/main/java/com/esca/escahp/user/entity/User.java
+++ escahp/src/main/java/com/esca/escahp/user/entity/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Getter
 @Entity
