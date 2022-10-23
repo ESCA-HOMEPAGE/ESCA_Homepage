71c362dc4630c558384891fa7a98638872359a0e
DyeonKim <vlxjdus@gmail.com>
2022-10-23 17:57:25 +0900

    feat: 테스트용 회원 데이터 생성

--- escahp/src/test/java/com/esca/escahp/DataLoader.java
+++ escahp/src/test/java/com/esca/escahp/DataLoader.java
@@ -14,6 +14,28 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     public static final StudyBoard board1 = new StudyBoard("title", "content", "writer", "category", "file");
     public static final User user = new User("yjahn", "pw", 19, "rs", "ahn",4, "email@mail.com", "img", "this is pr!");
+    public static final User user1 = User.builder()
+        .userId("testId1")
+        .password("test1234!")
+        .email("test1@test.com")
+        .generation(20)
+        .nickname("nickOne")
+        .name("test")
+        .profileImg("profile1.jpg")
+        .pr("hello")
+        .rank(2)
+        .build();
+    public static final User user2 = User.builder()
+        .userId("testId2")
+        .password("test1234!")
+        .email("test2@test.com")
+        .generation(19)
+        .nickname("nickTwo")
+        .name("test")
+        .profileImg("profile2.jpg")
+        .pr("hi")
+        .rank(1)
+        .build();
 
     public static String token;
     private final StudyRepository studyRepository;
@@ -30,6 +52,8 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args){
         studyRepository.save(board1);
         userRepository.save(user);
+        userRepository.save(user1);
+        userRepository.save(user2);
         token = jwtTokenProvider.createToken(user.getUserId());
         System.out.println("=============" + token);
     }
