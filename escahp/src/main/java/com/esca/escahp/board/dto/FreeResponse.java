8bf97bc6611f5b4df165a0b25c6fad7e1276e779
DyeonKim <vlxjdus@gmail.com>
2022-10-23 18:37:37 +0900

    docs : 자유게시판 스웨거에 필요한 설명 추가

--- escahp/src/main/java/com/esca/escahp/board/dto/FreeResponse.java
+++ escahp/src/main/java/com/esca/escahp/board/dto/FreeResponse.java
@@ -2,37 +2,49 @@ package com.esca.escahp.board.dto;
 
 import com.esca.escahp.board.entity.FreeBoard;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@ApiModel(value = "FreeBoard : 게시물 응답 정보", description = "자유 게시판의 게시물 응답 정보를 나타낸다.")
+@ApiModel(value = "FreeResponse : 게시물 응답 정보", description = "자유 게시판의 게시물 응답 정보를 나타낸다.")
 @Getter
 @NoArgsConstructor
 public class FreeResponse {
-
+    @ApiModelProperty(value = "게시물 번호(PK)")
     private Long id;
 
+    @ApiModelProperty(value = "게시물 제목")
     private String title;
 
+    @ApiModelProperty(value = "게시물 내용")
     private String content;
 
+    @ApiModelProperty(value = "작성자 아이디")
     private String writer;
 
+    @ApiModelProperty(value = "첨부파일")
     private String file;
 
+    @ApiModelProperty(value = "작성일자")
     private LocalDateTime createdAt;
 
+    @ApiModelProperty(value = "수정일자")
     private LocalDateTime updatedAt;
 
+    @ApiModelProperty(value = "삭제일자")
     private LocalDateTime deletedAt;
 
+    @ApiModelProperty(value = "삭제여부")
     private String deleteYn;
 
+    @ApiModelProperty(value = "조회수")
     private long viewCnt;
 
+    @ApiModelProperty(value = "신고수")
     private int report;
 
+    @ApiModelProperty(value = "좋아요 수")
     private int likes;
 
     public FreeResponse(FreeBoard freeBoard) {
