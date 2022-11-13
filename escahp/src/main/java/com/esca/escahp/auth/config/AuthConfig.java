7f21a5264f5fb6b759a0e7421cd984798526903a
Choonsik <48675973+RinSabbit@users.noreply.github.com>
2022-11-13 17:00:59 +0900

    feat: change interceptor pattern, swagger config (#51)

--- escahp/src/main/java/com/esca/escahp/auth/config/AuthConfig.java
+++ escahp/src/main/java/com/esca/escahp/auth/config/AuthConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @AllArgsConstructor
 public class AuthConfig implements WebMvcConfigurer {
@@ -14,8 +16,8 @@ public class AuthConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        List<String> addPatterns = List.of("/user/**", "/schedules/**", "/free/**", "/study/**", "/gallery/**", "/keeper/**", "/notice/**");
         registry.addInterceptor(new MemberInterceptor(jwtTokenProvider))
-                .addPathPatterns("/**")
-                .excludePathPatterns("/auth/**");
+                .addPathPatterns(addPatterns);
     }
 }
--- escahp/src/main/java/com/esca/escahp/config/SwaggerConfig.java
+++ escahp/src/main/java/com/esca/escahp/config/SwaggerConfig.java
@@ -2,16 +2,22 @@ package com.esca.escahp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.web.servlet.function.RequestPredicates.GET;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
@@ -32,7 +38,30 @@ public class SwaggerConfig {
 			.select()
 			.apis(RequestHandlerSelectors.basePackage("com.esca.escahp"))
 			.paths(PathSelectors.any())
-			.build();
+			.build()
+			.securityContexts(Arrays.asList(securityContext()))
+			.securitySchemes(Arrays.asList(apiKey()));
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return springfox
+				.documentation
+				.spi.service
+				.contexts
+				.SecurityContext
+				.builder()
+				.securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
 	}
 	@Bean
 	RouterFunction<ServerResponse> routerFunction() {
