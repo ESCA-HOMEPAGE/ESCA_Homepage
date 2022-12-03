0dd2af196fb8b4032387a3478717f932318d4f4c
RinSabbit <celena1004dev@gmail.com>
2022-12-04 02:38:20 +0900

    refactor: add service interface method with skeletion dtos

--- escahp/src/main/java/com/esca/escahp/timetable/I_TimeTableService.java
+++ escahp/src/main/java/com/esca/escahp/timetable/I_TimeTableService.java
@@ -1,5 +1,18 @@
 package com.esca.escahp.timetable;
 
+import com.esca.escahp.timetable.dto.*;
+
+import java.util.List;
+
 public interface I_TimeTableService {
+    List<TimeTableListResponse> getTimeTableList();
+    TimeTableResponse selectTimeTable(long id);
+    TimeTableResponse addTimeTable(TimeTableRequest request);
+    TimeTableResponse updateTimeTable(long id, TimeTableRequest request);
+    void deleteTimeTable(long id);
+    KeeperResponse addKeeper(KeeperRequest request);
+    KeeperResponse updateKeeper(long id, KeeperRequest request);
+    void deleteKeeper(long id);
+    void changeShowStatus(long id);
     
 }
